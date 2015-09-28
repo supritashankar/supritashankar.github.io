ebe3d3f8230873b8b42c548a1101f8f1c18c2767
suprita shankar <suprita.shankar@gmail.com>
2015-09-27 18:04:46 -0700

    tidy

--- zc/zeroclipboard-starter-snippet-codepen/js/index.js
+++ zc/zeroclipboard-starter-snippet-codepen/js/index.js
@@ -8,19 +8,10 @@ ZeroClipboard.config({
 var client = new ZeroClipboard($("button[data-clipboard-text]"));
 
 client.on("error", function(e) {
-    //log("ERROR! [" + e.name + "] " + e.message);
 });
 
 client.on("ready", function(e) {
-    //log("Ready!");
 
     client.on("aftercopy", function(e) {
-        //log((e.success["text/plain"] ? "Copied" : "FAILED to copy") + ": " + e.data["text/plain"]);
-        showActive(e.target);
     });
 });
-
-$(".zc-state-updater").on("click", function() {
-  $("#zcState pre").text(JSON.stringify(ZeroClipboard.state(), null, 2));
-});
-$("#zcState pre").text(JSON.stringify(ZeroClipboard.state(), null, 2));
