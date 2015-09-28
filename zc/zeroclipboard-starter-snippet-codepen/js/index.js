2189255f7efc58b22edb53578c793cba8d67041f
suprita shankar <suprita.shankar@gmail.com>
2015-09-28 15:29:34 -0500

    see if this works

--- zc/zeroclipboard-starter-snippet-codepen/js/index.js
+++ zc/zeroclipboard-starter-snippet-codepen/js/index.js
@@ -1,12 +1,10 @@
 //
 // Begin ZeroClipboard stuff
 //
-function trigger(){
-	var client = new ZeroClipboard($("span[data-clipboard-text]"));
+	var client = new ZeroClipboard($("button[data-clipboard-text]"));
 
 	client.on("ready", function(e) {
 		console.log('in ready1');
 	    client.on("aftercopy", function(e) {
 	    });
 	});
-}
