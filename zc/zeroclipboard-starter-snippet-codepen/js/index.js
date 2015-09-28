37832de5cf7b267f0dee3363482aadd1cbc8029e
suprita shankar <suprita.shankar@gmail.com>
2015-09-27 19:47:41 -0700

    lets see if this works

--- zc/zeroclipboard-starter-snippet-codepen/js/index.js
+++ zc/zeroclipboard-starter-snippet-codepen/js/index.js
@@ -5,7 +5,7 @@
 var client = new ZeroClipboard($("button[data-clipboard-text]"));
 
 client.on("ready", function(e) {
-
+	console.log('in ready');
     client.on("aftercopy", function(e) {
     });
 });
