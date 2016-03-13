2896a39d953b681c43d3d0f9294e407e12b7d719
suprita shankar <suprita.shankar@gmail.com>
2016-03-12 17:31:11 -0800

    put all of the previous files in backup

---
+++ backup2/zc/zeroclipboard-starter-snippet-codepen/js/index.js
@@ -0,0 +1,10 @@
+//
+// Begin ZeroClipboard stuff
+//
+	var client = new ZeroClipboard($("button[data-clipboard-text]"));
+
+	client.on("ready", function(e) {
+		console.log('in ready1');
+	    client.on("aftercopy", function(e) {
+	    });
+	});
